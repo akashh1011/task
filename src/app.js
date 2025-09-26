f84c65024e790b2f5ba86aba595e72e00601c665
Akash Prajapati <prajapatiaakash816@gmail.com>
2025-09-26 22:27:08 +0530

    fix: remove duplicate CORS middleware initialization in app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import express from "express"
 import cors from "cors"
 import productRouter from "../src/routes/product.route.js"
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
 
-app.use(cors())
+
 
 
 //route initialize
